8a4f234b1d330de19f36f064199e814c81ab6672
João Augusto Câmara Lopes Costa <camarajoaoaugusto@gmail.com>
2025-09-15 22:57:21 -0300

    feat: Implementa CalculadoraIMC e testes

--- src/test/java/exercicio02/CalculadoraIMCTest.java
+++ src/test/java/exercicio02/CalculadoraIMCTest.java
@@ -4,10 +4,28 @@
  */
 package exercicio02;
 
-/**
- *
- * @author camar
- */
-public class CalculadoraIMCTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraIMCTest {
     
+    CalculadoraIMC calc = new CalculadoraIMC();
+
+    @Test
+    void testCalculoIMC_CasoNormal() {
+        // Testa um caso de IMC normal
+        assertEquals(22.86, calc.calcular(70, 1.75), 0.01);
+    }
+
+    @Test
+    void testClassificacao_AbaixoDoPeso() {
+        // Testa a classificação para IMC abaixo do peso
+        assertEquals("Abaixo do peso", calc.getClassificacao(18.4));
+    }
+
+    @Test
+    void testClassificacao_Sobrepeso() {
+        // Testa a classificação para Sobrepeso
+        assertEquals("Sobrepeso", calc.getClassificacao(27.5));
+    }
 }
