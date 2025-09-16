d44630ca70b53320b971f2ca063f55c4fcc9c830
João Augusto Câmara Lopes Costa <camarajoaoaugusto@gmail.com>
2025-09-15 22:38:27 -0300

    feat: Implementa ConversorTemperatura e testes unitários

--- src/main/java/exercicio01/ConversorTemperatura.java
+++ src/main/java/exercicio01/ConversorTemperatura.java
@@ -9,5 +9,10 @@ package exercicio01;
  * @author camar
  */
 public class ConversorTemperatura {
-    
+    public static double celsiusParaFahrenheit(double celsius) {
+        return (celsius * 9.0 / 5.0) + 32;
+    }
+    public static double fahrenheitParaCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5.0 / 9.0;
+    }
 }
